68831d396b3f63ec2008155da167b2273b8292dc
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2022-09-10 01:23:19 +0300

    first css exercise

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import "./style.css";
+
+function App() {
+  return (
+    <div class="main">
+      <h1>Welcome to the Beach</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi officiis
+        ipsum officia numquam expedita ullam.
+      </p>
+      <Button class="btn" variant="contained">
+        Read More
+      </Button>
+      <div class="p">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus
+        impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat,
+        placeat iste facere, aspernatur ex cum veritatis laudantium, officia,
+        non porro exercitationem incidunt quis dolore? Officia ex accusamus
+        expedita optio, voluptatem minus? In maiores omnis aperiam earum ab
+        molestiae beatae laborum blanditiis incidunt, delectus dolor, id
+        voluptates optio aspernatur aliquam saepe atque labore? Tempore
+        reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente
+        in numquam blanditiis, enim, illo error nulla incidunt quos quidem
+        ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore
+        commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti
+        quae.
+      </div>
+      <div class="p-w">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minus
+        impedit maxime, quae soluta quis cumque perferendis! Doloribus quaerat,
+        placeat iste facere, aspernatur ex cum veritatis laudantium, officia,
+        non porro exercitationem incidunt quis dolore? Officia ex accusamus
+        expedita optio, voluptatem minus? In maiores omnis aperiam earum ab
+        molestiae beatae laborum blanditiis incidunt, delectus dolor, id
+        voluptates optio aspernatur aliquam saepe atque labore? Tempore
+        reprehenderit ab ipsam perspiciatis ut, provident perferendis sapiente
+        in numquam blanditiis, enim, illo error nulla incidunt quos quidem
+        ratione repellat ipsa molestias veritatis? Mollitia, fugit dolore
+        commodi porro repudiandae atque, eos, ipsum quam culpa fuga deleniti
+        quae.
+      </div>
+      <div class="all-p">
+        <div class="p-f">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolorum
+          est, molestias dolores quis sunt nobis temporibus veritatis libero
+          odio!
+        </div>
+        <div class="p-s">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolorum
+          est, molestias dolores quis sunt nobis temporibus veritatis libero
+          odio!
+        </div>
+        <div class="p-t">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolorum
+          est, molestias dolores quis sunt nobis temporibus veritatis libero
+          odio!
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
